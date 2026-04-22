fa9379466bdf15e60571dad9f4d192af0f64a6a7
suman <sumanthegde32@gmail.com>
2026-04-22 22:14:00 +0530

    Fixed CORS to allow Vercel

--- src/main/java/com/localbridge/market_nexus/controller/JobPostingController.java
+++ src/main/java/com/localbridge/market_nexus/controller/JobPostingController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/jobs")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class JobPostingController {
 
     private final JobPostingRepository jobPostingRepository;
--- src/main/java/com/localbridge/market_nexus/controller/ShopController.java
+++ src/main/java/com/localbridge/market_nexus/controller/ShopController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/shops")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ShopController {
 
     private final ShopRepository shopRepository;
